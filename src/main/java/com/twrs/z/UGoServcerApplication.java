7d3d509e2e6da6a509acbefb820545388188c157
YingjiaZhang <zhyingjia@163.com>
2017-08-29 10:52:05 +0800

    init

---
+++ src/main/java/com/twrs/z/UGoServcerApplication.java
@@ -0,0 +1,12 @@
+package com.twrs.z;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UGoServcerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UGoServcerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/twrs/z/UGoServcerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.twrs.z;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UGoServcerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
